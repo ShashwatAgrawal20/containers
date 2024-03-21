5651f76146ecf9266a52dd0d326fdb6dd9eec38c
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2024-03-21 22:42:18 +0530

    feat: NEWPID namespace implementation.
    - The run method now spawn a `child` process/function and the `child`
      now runs the actual command with arguments.
      Did this to actually use the NEWPID namespace, so e.g the spawn
      `/bin/bash` shell will now have a different pid than a parent(likely
      1)
    
    - I am aware of a shitty bug that exists, which is the user can directly invoke the
      command with the `child` as the argument insted of `run` and will skip the invokation of
      the `child` process and other `namespace` creation which is a big nono.

--- src/core.rs
+++ src/core.rs
@@ -3,13 +3,22 @@ use nix::unistd::sethostname;
 use std::process::{exit, Command};
 
 pub fn run(args: &Vec<String>) {
-    //! For now this function just takes the args Vector and try to run the `command` in that.
-    //! **NOTE:** Before running the command it also tries to create a testing `UTS namespace` and set the
-    //! hostname to `container`.
-    println!("running {:?}", &args[2..]);
+    //! Sets up a new UTS and PID namespace and executes a child process within that namespace.
+    //!
+    //! This function creates a new UTS and PID namespace using the Linux kernel's `unshare` system
+    //! call and sets the hostname to "container". It then spawns a child process using the same
+    //! binary (`/proc/self/exe`) with the "child" argument and the provided command and arguments.
+    //!
+    //! The `child` function is called when the child process is executed, which then runs the
+    //! specified command within the namespace.
+    //!
+    //! # Arguments
+    //!
+    //! * `args` - A vector of strings representing the command-line arguments passed to the program.
+    println!("process id of parent:- {}", std::process::id());
 
     // ----------------- Some shitty NAMESPACES testing -----------------
-    if let Err(err) = unshare(CloneFlags::CLONE_NEWUTS) {
+    if let Err(err) = unshare(CloneFlags::CLONE_NEWUTS | CloneFlags::CLONE_NEWPID) {
         eprintln!("Failed to create UTS namespace: {}", err);
         exit(1);
     }
@@ -18,6 +27,28 @@ pub fn run(args: &Vec<String>) {
         exit(1);
     }
     // ----------------- Some shitty NAMESPACES testing -----------------
+
+    let _output = Command::new("/proc/self/exe")
+        .args(format!("child {} {}", &args[2], &args[3..].join(" ")).split_whitespace())
+        .spawn()
+        .expect("failed to execute process")
+        .wait()
+        .expect("failed to wait on process");
+}
+
+pub fn child(args: &Vec<String>) {
+    //! Executes a command within the current UTS and PID namespace.
+    //!
+    //! This shitty function is meant to be called when the `run` function spawn the `child`
+    //! process. It takes the command and its arguments and executes it within the newly created
+    //! `namespaces`.
+    //!
+    //! # Arguments
+    //!
+    //! * `args` - A vector of strings representing the command and its arguments.
+    println!("running {:?}", &args[2..]);
+
+    println!("process id in child:- {}", std::process::id());
 
     let _output = Command::new(&args[2])
         .args(&args[3..])
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 mod core;
 
+/// The main entry point of the program.
+///
+/// This function collects command-line arguments and matches the first argument to determine
+/// which function to call. If the first argument is "run", it calls the `run` function to set
+/// up namespaces and execute a child process. If the first argument is "child", it calls the
+/// `child` function to execute a command within the current namespace. If the first argument
+/// doesn't match any of these options, it prints "bad command".
+///
+///
+/// I am aware of a shitty bug that exists, which is the user can directly invoke the
+/// command with the `child` as the argument insted of `run` and will skip the invokation of
+/// the `child` process and other `namespace` creation which is a big nono.
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     match args.get(1).map(|x| x.as_str()) {
         Some("run") => core::run(&args),
+        Some("child") => core::child(&args),
         _ => println!("bad command"),
     }
 }
