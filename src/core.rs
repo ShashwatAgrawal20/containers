3a7eb6f71a2c0cc697a23e641b617838e69d5711
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2024-03-23 19:35:39 +0530

    feat!: exploring some pivot_root stuff.
    - This pivot root implementation also fixes the issue from the previous
      commit of the containers mount-points getting exposed to the host.
    
    - Might change the implementation later as currently this implementation
      is pretty garbage.
    
    - Code feels a little too janky, will refactor it soon.

--- src/core.rs
+++ src/core.rs
@@ -1,9 +1,13 @@
 use nix::{
-    mount::{mount, umount, MsFlags},
+    mount::{mount, umount, umount2, MntFlags, MsFlags},
     sched::{unshare, CloneFlags},
-    unistd::{chdir, chroot, sethostname},
+    unistd::{chdir, pivot_root, sethostname},
 };
-use std::process::{exit, Command};
+use std::{
+    fs::{create_dir_all, remove_dir},
+    path::PathBuf,
+    process::{exit, Command},
+};
 
 pub fn run(args: &Vec<String>) {
     println!("process id of parent:- {}", std::process::id());
@@ -33,18 +37,65 @@ pub fn child(args: &Vec<String>) {
     println!("running {:?}", &args[2..]);
     println!("process id in child:- {}", std::process::id());
 
+    // TODO: Implement cgroups
+
     let path = std::env::current_dir().unwrap();
     let abs_path = format!("{}/{}", path.display(), "rootfs");
+    let abs_path_pathbuf = PathBuf::from(&abs_path);
 
-    if let Err(err) = chroot(abs_path.as_str()) {
-        eprintln!("Failed to chroot: {}", err);
+    // ----------------- Shitty pivot_root implementation -----------------
+    if let Err(err) = mount(
+        None::<&str>,
+        "/",
+        None::<&str>,
+        MsFlags::MS_REC | MsFlags::MS_PRIVATE,
+        None::<&str>,
+    ) {
+        eprintln!("Failed to remount root as private: {}", err);
         exit(1);
-    }
+    };
 
-    if let Err(err) = chdir("/") {
-        eprintln!("Failed to change root directory: {}", err);
+    if let Err(err) = mount(
+        Some(&abs_path_pathbuf),
+        &abs_path_pathbuf,
+        None::<&str>,
+        MsFlags::MS_BIND | MsFlags::MS_PRIVATE,
+        None::<&str>,
+    ) {
+        eprintln!("Failed to remount root as private: {}", err);
         exit(1);
-    }
+    };
+
+    if let Err(err) = create_dir_all(format!("{}/oldrootfs", abs_path)) {
+        eprintln!("Failed to create oldrootfs directory: {}", err);
+        exit(1);
+    };
+
+    if let Err(err) = pivot_root(
+        &abs_path_pathbuf,
+        &PathBuf::from(format!("{}/oldrootfs", abs_path)),
+    ) {
+        eprintln!("Failed to pivot_root: {}", err);
+        exit(1);
+    };
+
+    if let Err(err) = umount2(
+        &std::path::PathBuf::from("/oldrootfs"),
+        MntFlags::MNT_DETACH,
+    ) {
+        eprintln!("Failed to detach oldrootfs: {}", err);
+        exit(1);
+    };
+
+    if let Err(err) = remove_dir(&PathBuf::from(format!("/oldrootfs"))) {
+        eprintln!("Failed to remove oldrootfs: {}", err);
+        exit(1);
+    };
+    if let Err(err) = chdir(&PathBuf::from("/")) {
+        eprintln!("Failed to change directory to /: {}", err);
+        exit(1);
+    };
+    // ----------------- Shitty pivot_root implementation -----------------
 
     if let Err(err) = mount(
         Some("proc"),
@@ -55,7 +106,7 @@ pub fn child(args: &Vec<String>) {
     ) {
         eprintln!("Failed to mount proc filesystem: {}", err);
         exit(1);
-    }
+    };
 
     let _output = Command::new(&args[2])
         .args(&args[3..])
@@ -67,5 +118,5 @@ pub fn child(args: &Vec<String>) {
     if let Err(err) = umount("/proc") {
         eprintln!("Failed to unmount proc filesystem: {}", err);
         exit(1);
-    }
+    };
 }
