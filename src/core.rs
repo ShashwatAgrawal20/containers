bfc7f6ce4aeca133fd4345226cf42757f2133399
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2024-03-24 23:20:39 +0530

    feat(breaking changes)!: rootless containers using user namespace
    - This Shitty implementation currently fails at gid_mapping.
    
    - reverted back from pivot_root to normal chroot for temporarily.
    
    - currenty the file write to uid_map is successfull, idk why it's
      failing for those shitty gid_map.
    
    - might have to look into the user_namespace man page in detail.
    
    - when chrooting in the rootfs it seems like we are the root but we
      aren't. If we try running some command which actually needs the root
      previleges it fails miserably.

--- src/core.rs
+++ src/core.rs
@@ -1,28 +1,65 @@
 use nix::{
-    mount::{mount, umount, umount2, MntFlags, MsFlags},
+    mount::{mount, umount, MsFlags},
     sched::{unshare, CloneFlags},
-    unistd::{chdir, pivot_root, sethostname},
+    unistd::{chdir, chroot, sethostname},
 };
+#[allow(unused_imports)]
 use std::{
     fs::{create_dir_all, remove_dir},
     path::PathBuf,
     process::{exit, Command},
 };
 
+use std::fs::File;
+use std::io::Write;
+
 pub fn run(args: &Vec<String>) {
     println!("process id of parent:- {}", std::process::id());
 
-    if let Err(err) =
-        unshare(CloneFlags::CLONE_NEWUTS | CloneFlags::CLONE_NEWPID | CloneFlags::CLONE_NEWNS)
-    {
+    let uid = nix::unistd::getuid();
+    let gid = nix::unistd::getgid();
+
+    if let Err(err) = unshare(
+        CloneFlags::CLONE_NEWUTS
+            | CloneFlags::CLONE_NEWPID
+            | CloneFlags::CLONE_NEWNS
+            | CloneFlags::CLONE_NEWUSER,
+    ) {
         eprintln!("Failed to create namespaces: {}", err);
         exit(1);
     }
+    println!(
+        "uid after namespace creation {:?}",
+        nix::unistd::getuid().as_raw()
+    );
 
     if let Err(err) = sethostname("container") {
         eprintln!("Failed to set hostname: {}", err);
         exit(1);
     }
+
+    let pid = std::process::id();
+
+    if let Ok(mut uid_map) = File::create(format!("/proc/{}/{}", pid, "uid_map")) {
+        if let Err(err) = uid_map.write_all(format!("0 {} 1", uid.as_raw()).as_bytes()) {
+            eprintln!("Failed to write uid_map {}", err);
+            exit(1);
+        }
+    } else {
+        eprintln!("Failed to create uid_map");
+        exit(1);
+    }
+
+    // TODO: fix this shitty erroneous code
+    // if let Ok(mut gid_map) = File::create(format!("/proc/{}/{}", pid, "gid_map")) {
+    //     if let Err(err) = gid_map.write_all(format!("0 {} 1", gid.as_raw()).as_bytes()) {
+    //         eprintln!("Failed to write gid_map {}", err);
+    //         exit(1);
+    //     }
+    // } else {
+    //     eprintln!("Failed to create gid_map");
+    //     exit(1);
+    // }
 
     // The `/proc/self/exe` is a symbolic link to the current process's executable.
     let _output = Command::new("/proc/self/exe")
@@ -37,65 +74,18 @@ pub fn child(args: &Vec<String>) {
     println!("running {:?}", &args[2..]);
     println!("process id in child:- {}", std::process::id());
 
-    // TODO: Implement cgroups
-
     let path = std::env::current_dir().unwrap();
     let abs_path = format!("{}/{}", path.display(), "rootfs");
-    let abs_path_pathbuf = PathBuf::from(&abs_path);
 
-    // ----------------- Shitty pivot_root implementation -----------------
-    if let Err(err) = mount(
-        None::<&str>,
-        "/",
-        None::<&str>,
-        MsFlags::MS_REC | MsFlags::MS_PRIVATE,
-        None::<&str>,
-    ) {
-        eprintln!("Failed to remount root as private: {}", err);
+    if let Err(err) = chroot(abs_path.as_str()) {
+        eprintln!("Failed to chroot: {}", err);
         exit(1);
-    };
+    }
 
-    if let Err(err) = mount(
-        Some(&abs_path_pathbuf),
-        &abs_path_pathbuf,
-        None::<&str>,
-        MsFlags::MS_BIND | MsFlags::MS_PRIVATE,
-        None::<&str>,
-    ) {
-        eprintln!("Failed to remount root as private: {}", err);
+    if let Err(err) = chdir("/") {
+        eprintln!("Failed to change root directory: {}", err);
         exit(1);
-    };
-
-    if let Err(err) = create_dir_all(format!("{}/oldrootfs", abs_path)) {
-        eprintln!("Failed to create oldrootfs directory: {}", err);
-        exit(1);
-    };
-
-    if let Err(err) = pivot_root(
-        &abs_path_pathbuf,
-        &PathBuf::from(format!("{}/oldrootfs", abs_path)),
-    ) {
-        eprintln!("Failed to pivot_root: {}", err);
-        exit(1);
-    };
-
-    if let Err(err) = umount2(
-        &std::path::PathBuf::from("/oldrootfs"),
-        MntFlags::MNT_DETACH,
-    ) {
-        eprintln!("Failed to detach oldrootfs: {}", err);
-        exit(1);
-    };
-
-    if let Err(err) = remove_dir(&PathBuf::from(format!("/oldrootfs"))) {
-        eprintln!("Failed to remove oldrootfs: {}", err);
-        exit(1);
-    };
-    if let Err(err) = chdir(&PathBuf::from("/")) {
-        eprintln!("Failed to change directory to /: {}", err);
-        exit(1);
-    };
-    // ----------------- Shitty pivot_root implementation -----------------
+    }
 
     if let Err(err) = mount(
         Some("proc"),
@@ -106,7 +96,7 @@ pub fn child(args: &Vec<String>) {
     ) {
         eprintln!("Failed to mount proc filesystem: {}", err);
         exit(1);
-    };
+    }
 
     let _output = Command::new(&args[2])
         .args(&args[3..])
@@ -118,5 +108,5 @@ pub fn child(args: &Vec<String>) {
     if let Err(err) = umount("/proc") {
         eprintln!("Failed to unmount proc filesystem: {}", err);
         exit(1);
-    };
+    }
 }
